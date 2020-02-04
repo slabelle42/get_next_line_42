01040af8fe1acea94b26b7b73d8edb11746e6877
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-02-04 16:38:49 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -17,8 +17,6 @@
 # include <stdlib.h>
 # include <unistd.h> //io.h
 
-# define BUFFER_SIZE 32
-
 int		get_next_line(int fd, char **line);
 char	*gnl_strjoin(char const *s1, char const *s2);
 size_t	gnl_strlen(const char *str);
