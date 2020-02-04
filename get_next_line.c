d0ce4603b75dcf764753e7c7e31a3fba159ca468
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-02-04 15:13:26 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,9 +12,25 @@
 
 #include "get_next_line.h"
 
-int		gnl_check_errors(int fd, char **line, char **save)
+char	*gnl_join_and_free(int fd, char *save)
 {
-	if (fd == -1 || !line)
+	char			*to_free;
+	char			buffer[BUFFER_SIZE + 1];
+	int				nbytes;
+
+	to_free = save;
+	while ((nbytes = read(fd, buffer, BUFFER_SIZE)))
+	{
+		buffer[nbytes] = '\0';
+		save = gnl_strjoin(save, buffer);
+	}
+	free(to_free);
+	return (save);
+}
+
+int		gnl_verify_errors_malloc(int fd, char **line, char **save)
+{
+	if (fd == -1 || !line || BUFFER_SIZE < 1)
 		return (-1);
 	if (!*save)
 	{
@@ -26,18 +42,13 @@ int		gnl_check_errors(int fd, char **line, char **save)
 
 int		get_next_line(int fd, char **line)
 {
-	char			buffer[BUFFER_SIZE + 1];
 	static char		*save;
-	int				nbytes;
 	int				i;
 
-	if (gnl_check_errors(fd, line, &save) == -1)
+	if (gnl_verify_errors_malloc(fd, line, &save) == -1)
 		return (-1);
-	while ((nbytes = read(fd, buffer, BUFFER_SIZE)))
-	{
-		buffer[nbytes] = '\0';
-		save = gnl_strjoin(save, buffer);
-	}
+	
+	save = gnl_join_and_free(fd, save);
 	i = 0;
 	if (save[i])
 	{
