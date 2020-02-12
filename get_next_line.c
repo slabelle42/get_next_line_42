420922b0b17a54b443d721bb62c7c03ae19d0d48
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-02-12 23:28:39 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-void	free_and_null(char **to_free)
+static void		free_and_null(char **to_free)
 {
 	if (to_free && *to_free)
 	{
@@ -21,48 +21,48 @@ void	free_and_null(char **to_free)
 	}
 }
 
-char	*join_buffer(char *save, char *buffer)
+static char		*join_buffer(char *save, char *buffer)
 {
 	size_t			len_buffer;
 	size_t			len_save;
 	size_t			len;
 	char			*join;
 
-	len_buffer = ft_strlen(buffer);
+	len_buffer = gnl_strlen(buffer);
 	len_save = 0;
 	if (save)
-		len_save = ft_strlen(save);
+		len_save = gnl_strlen(save);
 	len = len_buffer + len_save;
 	if (!(join = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	if (save)
 	{
-		ft_strlcpy(join, save, (len_save + 1));
+		gnl_strlcpy(join, save, (len_save + 1));
 		free_and_null(&save);
 	}
-	ft_strlcpy((join + len_save), buffer, (len_buffer + 1));
+	gnl_strlcpy((join + len_save), buffer, (len_buffer + 1));
 	return (join);
 }
 
-int		line_saved(char **save, char **line)
+static int		line_saved(char **save, char **line)
 {
 	char			*found;
 	size_t			end;
 	size_t			len;
 
 	found = NULL;
-	if ((found = ft_strchr(*save, '\n')))
+	if ((found = gnl_strchr(*save, '\n')))
 	{
 		end = found - *save;
-		len = ft_strlen(found);
-		*line = ft_substr(*save, 0, end);
-		ft_strlcpy(*save, (found + 1), (len + 1));
+		len = gnl_strlen(found);
+		*line = gnl_substr(*save, 0, end);
+		gnl_strlcpy(*save, (found + 1), (len + 1));
 		return (1);
 	}
 	return (0);
 }
 
-int		get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
 	char			buffer[BUFFER_SIZE + 1];
 	static char		*save;
@@ -81,9 +81,9 @@ int		get_next_line(int fd, char **line)
 			return (1);
 	}
 	if (save && *save)
-		*line = ft_strdup(save);
+		*line = gnl_strdup(save);
 	else if (nbytes == 0)
-		*line = ft_strdup("");
+		*line = gnl_strdup("");
 	free_and_null(&save);
 	return (nbytes);
 }
