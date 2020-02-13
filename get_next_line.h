b0ad4a0efaab7a7ef9a850220345bf07f4b6da34
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-02-13 12:27:53 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define MAX_FD 1024
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
